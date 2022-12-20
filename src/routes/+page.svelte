e91df805a63a630ddf907c770c6caba71056c485
mihirgarg01 <hepmihir@gmail.com>
2022-12-19 22:41:26 -0800

    added walls

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,50 +19,79 @@
 		</h2>
 	</section>
 
-	<section
-		class="striped mx-auto grid h-96 w-96 grid-cols-5 grid-rows-6 place-content-start items-center rounded-md border-[3px] border-gray-900  bg-tealish p-1  child:h-full child:w-full child:rounded-sm child:border-[2px] child:border-gray-200 child:text-center child:align-middle "
-	>
-		<div>1</div>
-		<div>2</div>
-		<div>3</div>
-		<div>4</div>
-		<div>5</div>
-		<div>6</div>
-		<div>7</div>
-		<div>8</div>
-		<div>9</div>
-		<div>10</div>
-		<div>11</div>
-		<div>12</div>
-		<div>13</div>
-		<div>14</div>
-		<div>15</div>
-		<div>16</div>
-		<div>17</div>
-		<div>18</div>
-		<div>19</div>
-		<div>20</div>
-		<div>21</div>
-		<div>22</div>
-		<div>23</div>
-		<div>24</div>
-		<div>25</div>
-		<div>26</div>
-		<div>27</div>
-		<div>28</div>
-		<div>29</div>
-		<div>30</div>
+	<section class="mx-auto w-max border-4">
+		<div
+			class="striped grid h-[384px] w-[320px] grid-cols-5 grid-rows-6 rounded-md bg-tealish p-1 outline outline-4 outline-offset-2 outline-gray-900  child:h-[64px] child:w-[64px]  child:border-[3px] child:border-gray-200 child:text-center "
+		>
+			<div>1</div>
+			<div>2</div>
+			<div>3</div>
+			<div>4</div>
+			<div>5</div>
+			<div>6</div>
+			<div class="border-3 rounded-sm border-x-4 border-r-[6px] border-gray-900 border-b-inherit">
+				7
+			</div>
+			<div>8</div>
+			<div>9</div>
+			<div>10</div>
+			<div>11</div>
+			<div
+				class="border-3 rounded-sm border-r-[3px] border-gray-900 border-y-inherit border-l-inherit"
+			>
+				12
+			</div>
+			<div
+				class="border-3 rounded-sm border-t-4 border-l-[3px] border-gray-900 border-r-inherit border-b-inherit"
+			>
+				13
+			</div>
+			<div class="border-3 rounded-sm border-t-4 border-gray-900 border-x-inherit border-b-inherit">
+				14
+			</div>
+			<div>15</div>
+			<div>16</div>
+			<div
+				class="border-3 rounded-sm border-l-[3px] border-b-[5px] border-gray-900 border-t-inherit border-r-inherit"
+			>
+				17
+			</div>
+			<div>18</div>
+			<div
+				class="border-3 rounded-sm border-t-4 border-l-[4px] border-gray-900 border-r-inherit border-b-inherit"
+			>
+				19
+			</div>
+			<div>20</div>
+			<div>21</div>
+			<div>22</div>
+			<div>23</div>
+			<div
+				class="border-3 rounded-sm border-r-[5px] border-gray-900 border-y-inherit border-l-inherit"
+			>
+				24
+			</div>
+			<div>25</div>
+			<div>26</div>
+			<div>27</div>
+			<div>28</div>
+			<div>29</div>
+			<div>30</div>
+		</div>
 	</section>
-	<!-- <div
-		class="striped grid grid-cols-3 rounded-sm border-[3px] border-gray-900 bg-tealish p-[1px] child:rounded-sm child:border-[2px] child:border-gray-100"
-	>
-		<div>Item 1</div>
-		<div>Item 2</div>
-		<div>Item 3</div>
-		<div>Item 1</div>
-		<div>Item 2</div>
-		<div>Item 3</div>
-	</div> -->
+
+	<!-- <section class="flex justify-center">
+		<div
+			class="striped grid h-[128px] w-[192px] grid-cols-3 bg-tealish outline outline-4 outline-gray-900 child:h-[64px] child:w-[64px] child:border-2"
+		>
+			<div>Item 1</div>
+			<div>Item 2</div>
+			<div>Item 3</div>
+			<div>Item 1</div>
+			<div>Item 2</div>
+			<div>Item 3</div>
+		</div>
+	</section> -->
 
 	<section class="flex justify-center ">
 		<h2 class="m-2 w-auto rounded-md border-4 border-gray-200 p-2 text-center font-body text-xl">
