3a78892b7592f4b09b416ddf92c91cbb70d5aaf4
ArjunSahlot <iarjun.sahlot@gmail.com>
2022-12-20 17:52:26 -0800

    starting vanilla

---
+++ index.js
@@ -0,0 +1,6 @@
+var img_guard = document.createElement("img");
+img_guard.src = "https://png.pngitem.com/pimgs/s/101-1010641_download-svg-download-png-police-officer-emoji-transparent.png";
+img_guard.setAttribute("width", "30px");
+img_guard.setAttribute("height", "30px");
+var src = document.getElementById("1");
+src.appendChild(img);
